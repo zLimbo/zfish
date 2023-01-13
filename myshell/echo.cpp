fe0f581b59f8f22b35efff080c2b28e5c1cbe62c
z_limbo <z_limbo@foxmail.com>
2023-01-13 12:00:27 +0800

    myshell echo

---
+++ myshell/echo.cpp
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+#include <cstdio>
+#include <cstring>
+
+int main(int argc, char* argv[]) {
+    for (int i = 1; i < argc; ++i) {
+        write(1, argv[i], strlen(argv[i]));
+        if (i + 1 < argc) {
+            write(1, " ", 1);
+        } else {
+            write(1, "\n", 1);
+        }
+    }
+
+    return 0;
+}
