fe17938946c8abb4e937d115c93028ed61a04bf4
zLimbo <67814424+zLimbo@users.noreply.github.com>
2023-06-26 03:05:44 +0000

    zfmt

--- zfmt/debug.h
+++ zfmt/debug.h
@@ -5,49 +5,44 @@
 #include <string>
 #include <vector>
 
-namespace zl
-{
+namespace zl {
 
-	enum COLOR
-	{
-		BLACE = 30,
-		RED,
-		GREEN,
-		YELLO,
-		BLUE,
-		FUCHSIA,
-		ULTRAMARINE,
-		WHITE
-	};
+enum COLOR {
+    BLACE = 30,
+    RED,
+    GREEN,
+    YELLO,
+    BLUE,
+    FUCHSIA,
+    ULTRAMARINE,
+    WHITE,
+};
 
-	template <typename T>
-	inline void print(const T x)
-	{
-		std::cout << x << std::endl;
-	}
+template <typename T>
+inline void print(const T x) {
+    std::cout << x << std::endl;
+}
 
-	template <typename T>
-	inline void print(const std::vector<T> v)
-	{
-		for (const T &x : v)
-			std::cout << x << " ";
-		std::cout << std::endl;
-	}
+template <typename T>
+inline void print(const std::vector<T> v) {
+    for (const T &x : v)
+        std::cout << x << " ";
+    std::cout << std::endl;
+}
 
-	template <typename T, typename... Args>
-	inline void print(const T x, Args... args)
-	{
-		std::cout << x << " ";
-		print(args...);
-	}
+template <typename T, typename... Args>
+inline void print(const T x, Args... args) {
+    std::cout << x << " ";
+    print(args...);
+}
 
-	template <typename... Args>
-	inline void print(COLOR color, Args... args)
-	{
-		std::cout << "\033[1;" << color << "m";
-		print(args...);
-		std::cout << "\033[0m";
-	}
+template <typename... Args>
+inline void print(COLOR color, Args... args) {
+    std::cout << "\033[1;" << color << "m";
+    print(args...);
+    std::cout << "\033[0m";
+}
 
-} // zl
-#endif //__PRINT_H__
+}  // namespace zl
+
+#endif  //__PRINT_H__
